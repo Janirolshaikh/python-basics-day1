ff9cda541bbee75834bfb12255ce71e0ae4d43d0
Janirolshaikh <janirol.shaikh28@gmail.com>
2025-12-27 12:55:54 +0530

    Add files via upload

---
+++ day1_variables.py
@@ -0,0 +1,11 @@
+name = "Janirol"
+age = 27        # change to your real age
+city = "Mumbai" # change to your city
+
+print("My name is", name)
+print("I am", age, "years old")
+print("I live in", city)
+
+a = 10
+b = 20
+print("Sum is:", a + b)
